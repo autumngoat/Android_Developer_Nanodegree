11b98700f7633d28302c56334628e3273c9d974a
autumngoat <wong.sung.jeh@gmail.com>
2018-04-24 17:17:17 -0400

    feat: Handle the RecyclerView in MainActivity.java
    Grab a handle to the object, connect it to a grid layout manager,
    and attach an adapter for the (as yet obtained) data to display

--- Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/MainActivity.java
+++ Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.full_dream.popularmoviesstage1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUMBER_OF_COLUMNS = 2;
+
+    // Following the tutorial code at:
+    // https://developer.android.com/guide/topics/ui/layout/recyclerview.html#java
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        // This setting is used to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView, which
+        // should be true of the movie posters based on the sample UI of Stage 2
+        mRecyclerView.setHasFixedSize(true);
+
+        // Use a Grid Layout Manager as per the rubric
+        mLayoutManager = new GridLayoutManager(this, NUMBER_OF_COLUMNS);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //TODO Specify an adapter
+        //mAdapter = new mAdapter(dataSet);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
