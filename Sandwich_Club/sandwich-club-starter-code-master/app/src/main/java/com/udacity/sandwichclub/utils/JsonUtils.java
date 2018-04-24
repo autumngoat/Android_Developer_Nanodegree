a6e17878f8c9769ef7ccdd3b2c530ca70312f45e
autumngoat <wong.sung.jeh@gmail.com>
2018-04-24 10:14:21 -0400

    refactor: Implement JSON parsing to make a Sandwich object\n\nImplement parseSandwichJson in JsonUtils.java to parse JSON to create a Sandwich object.

--- Sandwich_Club/sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ Sandwich_Club/sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,53 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
+    private static final String TAG = JsonUtils.class.getSimpleName();
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich breadMiddleBread = new Sandwich();
+        ArrayList<String> aliases = new ArrayList<>();
+        ArrayList<String> ingredients = new ArrayList<>();
+
+        try{
+            JSONObject sandwich = new JSONObject(json);
+
+            String name = sandwich.getString("mainName");
+
+            JSONArray akaArray = sandwich.getJSONArray("alsoKnownAs");
+            for(int i=0; i<akaArray.length(); i++){
+                String alias = akaArray.getString(i);
+                aliases.add(alias);
+            }
+
+            String description = sandwich.getString("description");
+
+            String image = sandwich.getString("image");
+
+            JSONArray ingredientsArray = sandwich.getJSONArray("ingredients");
+            for(int i=0; i<ingredientsArray.length(); i++){
+                String ingredient = ingredientsArray.getString(i);
+                ingredients.add(ingredient);
+            }
+
+            breadMiddleBread.setMainName(name);
+            breadMiddleBread.setAlsoKnownAs(aliases);
+            breadMiddleBread.setDescription(description);
+            breadMiddleBread.setImage(image);
+            breadMiddleBread.setIngredients(ingredients);
+        } catch (JSONException e){
+            Log.e("TAG", "Problem parsing the sandwich JSON " + e);
+        }
+
+        return breadMiddleBread;
     }
 }
