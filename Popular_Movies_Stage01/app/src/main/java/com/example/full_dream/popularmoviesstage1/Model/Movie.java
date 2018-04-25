5639836af670350911846695949004620db49c4a
autumngoat <wong.sung.jeh@gmail.com>
2018-04-25 14:51:07 -0400

    feat: Create Model package and add Movie data model

---
+++ Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/Model/Movie.java
@@ -0,0 +1,61 @@
+package com.example.full_dream.popularmoviesstage1.Model;
+
+/**
+ * Provides a data model to represent a Movie object to hold parsed JSON data.
+ */
+
+public class Movie {
+    private String title;
+    private String releaseDate;
+    private int poster; // drawable reference id
+    private float voteAvg;
+    private String plotSynopsis;
+
+    public Movie(String title, String releaseDate, int poster, float voteAvg, String plotSynopsis){
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.poster = poster;
+        this.voteAvg = voteAvg;
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getPoster() {
+        return poster;
+    }
+
+    public void setPoster(int poster) {
+        this.poster = poster;
+    }
+
+    public float getVoteAvg() {
+        return voteAvg;
+    }
+
+    public void setVoteAvg(float voteAvg) {
+        this.voteAvg = voteAvg;
+    }
+
+    public String getPlotSynopsis() {
+        return plotSynopsis;
+    }
+
+    public void setPlotSynopsis(String plotSynopsis) {
+        this.plotSynopsis = plotSynopsis;
+    }
+}
