3ba812e68a2a89b0fdbff7b41a2f864d3e97d6bb
autumngoat <wong.sung.jeh@gmail.com>
2018-04-26 10:32:45 -0400

    refactor: Use ButterKnife to replace findViewById calls

--- Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/MainActivity.java
+++ Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/MainActivity.java
@@ -5,33 +5,27 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.full_dream.popularmoviesstage1.model.Movie;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int NUMBER_OF_COLUMNS = 2;
 
-    // Following the tutorial code for initial setup of RecyclerView:
-    // https://developer.android.com/guide/topics/ui/layout/recyclerview.html#java
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-
+    @BindView(R.id.recyclerview) private RecyclerView mRecyclerView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
 
-        // This setting is used to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView, which
-        // should be true of the movie posters based on the sample UI of Stage 2
+        GridLayoutManager layoutManager = new GridLayoutManager(this, NUMBER_OF_COLUMNS);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
         mRecyclerView.setHasFixedSize(true);
-
-        // Use a Grid Layout Manager as per the rubric
-        mLayoutManager = new GridLayoutManager(this, NUMBER_OF_COLUMNS);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        //TODO Specify an adapter
-        //mAdapter = new mAdapter(dataSet);
-        mRecyclerView.setAdapter(mAdapter);
     }
 }
