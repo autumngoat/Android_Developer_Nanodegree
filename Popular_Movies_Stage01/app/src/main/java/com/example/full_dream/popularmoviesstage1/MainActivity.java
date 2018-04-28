27214f3b8e2776abcfc0c2b659f6f0477b24cd23
autumngoat <wong.sung.jeh@gmail.com>
2018-04-27 20:37:32 -0400

    feat: Implement DetailActivity and related XML
    Added copyright notice to whole project.
    Everything works except images still do not show up.
    Get your own API key.

--- Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/MainActivity.java
+++ Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/MainActivity.java
@@ -1,3 +1,50 @@
+/*
+ *
+ *  * PROJECT LICENSE
+ *  *
+ *  * This project was submitted by Seong Wang as part of the Android Developer Nanodegree at Udacity.
+ *  *
+ *  * As part of Udacity Honor code:
+ *  *   - I hereby confirm that all project submissions consist of my own work. Accordingly, I will document and cite the origins of any part(s) of my project submissions that were taken from websites, books, forums, blog posts, GitHub repositories, or any other source and explain why I used them for any part of my submission.
+ *  *     - Acceptable sources consist of:
+ *  *         - Unmodified or modified code from the Udacity courses
+ *  *         - A modest amount of unmodified or modified code from third-party sources with attribution
+ *  *     - NOT ACCEPTABLE: Any part of another student’s work, even with attribution
+ *  *   - I understand that Udacity will check my submission for plagiarism, and that failure to adhere to the Udacity Honor Code
+ *  *   may result in the cancellation of my enrollment without refund.
+ *  *   - I understand that I may be asked to explain my work in a video call with a Udacity Representative before my Nanodegree is
+ *  *   conferred.
+ *  *
+ *  * I, the author of the project, allow you to check the code as a reference so long as you are not a fellow Android Developer
+ *  * Nanodegree student, but if you submit it, it's your own responsibility if you get expelled.
+ *  *
+ *  * Copyright (c) 2018 Seong Wang
+ *  *
+ *  * Besides the above notice, the following license applies and this license notice must be included in all works derived from
+ *  * this project.
+ *  *
+ *  * MIT License
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  * SOFTWARE.
+ *
+ */
+
 package com.example.full_dream.popularmoviesstage1;
 
 import android.content.Context;
@@ -30,9 +77,7 @@ public class MainActivity extends AppCompatActivity implements PosterAdapter.Pos
     private static final String mostPopular = "popularity.desc";
     private static final String topRated = "vote_average.desc";
 
-    // Following the tutorial code at:
-    // https://developer.android.com/guide/topics/ui/layout/recyclerview.html#java
-    // ^Fuck that tutorial, looked back at my code for S05.01-Exercise-AsyncTaskLoader
+    // Looked back at my code for S05.01-Exercise-AsyncTaskLoader
     // for a sanity check
     private RecyclerView mRecyclerView;
     private PosterAdapter mPosterAdapter;
@@ -115,11 +160,11 @@ public class MainActivity extends AppCompatActivity implements PosterAdapter.Pos
     }
 
     @Override
-    public void onClick(String todoLater) {
+    public void onClick(String[] movieDetails) {
         Context context = this;
         Class destinationClass = DetailActivity.class;
         Intent intentToStartDetailActivity = new Intent(context, destinationClass);
-        intentToStartDetailActivity.putExtra(Intent.EXTRA_TEXT, todoLater);
+        intentToStartDetailActivity.putExtra(Intent.EXTRA_TEXT, movieDetails);
         startActivity(intentToStartDetailActivity);
     }
 }
--- Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/PosterAdapter.java
+++ Popular_Movies_Stage01/app/src/main/java/com/example/full_dream/popularmoviesstage1/PosterAdapter.java
@@ -1,14 +1,65 @@
+/*
+ *
+ *  * PROJECT LICENSE
+ *  *
+ *  * This project was submitted by Seong Wang as part of the Android Developer Nanodegree at Udacity.
+ *  *
+ *  * As part of Udacity Honor code:
+ *  *   - I hereby confirm that all project submissions consist of my own work. Accordingly, I will document and cite the origins of any part(s) of my project submissions that were taken from websites, books, forums, blog posts, GitHub repositories, or any other source and explain why I used them for any part of my submission.
+ *  *     - Acceptable sources consist of:
+ *  *         - Unmodified or modified code from the Udacity courses
+ *  *         - A modest amount of unmodified or modified code from third-party sources with attribution
+ *  *     - NOT ACCEPTABLE: Any part of another student’s work, even with attribution
+ *  *   - I understand that Udacity will check my submission for plagiarism, and that failure to adhere to the Udacity Honor Code
+ *  *   may result in the cancellation of my enrollment without refund.
+ *  *   - I understand that I may be asked to explain my work in a video call with a Udacity Representative before my Nanodegree is
+ *  *   conferred.
+ *  *
+ *  * I, the author of the project, allow you to check the code as a reference so long as you are not a fellow Android Developer
+ *  * Nanodegree student, but if you submit it, it's your own responsibility if you get expelled.
+ *  *
+ *  * Copyright (c) 2018 Seong Wang
+ *  *
+ *  * Besides the above notice, the following license applies and this license notice must be included in all works derived from
+ *  * this project.
+ *  *
+ *  * MIT License
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  * SOFTWARE.
+ *
+ */
+
 package com.example.full_dream.popularmoviesstage1;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.full_dream.popularmoviesstage1.model.Movie;
+import com.example.full_dream.popularmoviesstage1.utils.NetworkUtils;
 
 import java.util.ArrayList;
 
@@ -17,24 +68,19 @@ import java.util.ArrayList;
  */
 public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterAdapterViewHolder> {
     private ArrayList<Movie> mMovieData;
-    /*
-     * An on-click handler that we've defined to make it easy for an Activity to interface with
-     * our RecyclerView
-     */
+
+    // I adapted much of the Adapter code from my code for S05.01-Exercise-AsyncTaskLoader
     private final PosterAdapterOnClickHandler mClickHandler;
 
     /**
      * The interface that receives onClick messages.
      */
     public interface PosterAdapterOnClickHandler {
-        void onClick(String weatherForDay);
+        void onClick(String[] movieData);
     }
 
     /**
      * Creates a PosterAdapter.
-     *
-     * @param clickHandler The on-click handler for this adapter. This single handler is called
-     *                     when an item is clicked.
      */
     public PosterAdapter(PosterAdapterOnClickHandler clickHandler) {
         mClickHandler = clickHandler;
@@ -42,10 +88,6 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterAdap
 
     /**
      * Creates ViewHolders
-     *
-     * @param parent
-     * @param viewType
-     * @return
      */
     @NonNull
     @Override
@@ -68,8 +110,8 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterAdap
     @Override
     public void onBindViewHolder(@NonNull PosterAdapterViewHolder posterViewHolder, int position) {
         Movie movie = mMovieData.get(position);
-//        Uri uri = NetworkUtils.buildImgUri(movie.getPoster());
-//        Glide.with(mContext).load(uri).into(posterViewHolder.mPosterImageView);
+        Uri uri = NetworkUtils.buildImgUri(movie.getPoster());
+        Glide.with(posterViewHolder.itemView.getContext()).load(uri).into(posterViewHolder.mPosterImageView);
         posterViewHolder.mTitleTextView.setText(movie.getTitle());
     }
 
@@ -94,25 +136,31 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterAdap
      */
     public class PosterAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView mTitleTextView;
-//        ImageView mPosterImageView;
+        ImageView mPosterImageView;
 
+        /**
+         * Sets click listener on newly created empty ViewHolder
+         * @param view
+         */
         public PosterAdapterViewHolder(View view){
             super(view);
             mTitleTextView = (TextView)view.findViewById(R.id.tv_title);
-//            mPosterImageView = (ImageView)view.findViewById(R.id.iv_poster);
+            mPosterImageView = (ImageView)view.findViewById(R.id.iv_poster);
             view.setOnClickListener(this);
         }
 
         /**
-         * This gets called by the child views during a click.
+         * Sends selected View's associated Movie object to click-handler.
          *
-         * @param v The View that was clicked
+         * @param view The View that was clicked
          */
         @Override
-        public void onClick(View v) {
+        public void onClick(View view) {
             int adapterPosition = getAdapterPosition();
-            Movie movie = mMovieData.get(adapterPosition);
-            mClickHandler.onClick(movie.toString());
+            Movie selectedMovie = mMovieData.get(adapterPosition);
+            String[] movieData = {selectedMovie.getTitle(), selectedMovie.getReleaseDate(),
+                    Double.toString(selectedMovie.getVoteAvg()), selectedMovie.getPlotSynopsis()};
+            mClickHandler.onClick(movieData);
         }
     }
 }
