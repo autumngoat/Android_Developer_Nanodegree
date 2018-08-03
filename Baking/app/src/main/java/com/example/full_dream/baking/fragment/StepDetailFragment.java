2f44636169ffd1068d8b367f1b65d096a7f93296
autumngoat <wong.sung.jeh@gmail.com>
2018-08-02 22:24:20 -0400

    P4 refactor: Implement SimpleExoPlayer successfully.

--- Baking/app/src/main/java/com/example/full_dream/baking/fragment/StepDetailFragment.java
+++ Baking/app/src/main/java/com/example/full_dream/baking/fragment/StepDetailFragment.java
@@ -47,6 +47,8 @@ package com.example.full_dream.baking.fragment;
 
 // Android Imports
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -56,7 +58,27 @@ import android.view.View;
 import android.view.ViewGroup;
 
 // 3rd Party Imports - com - Baking
+import com.example.full_dream.baking.R;
+import com.example.full_dream.baking.databinding.FragmentStepDetailBinding;
 import com.example.full_dream.baking.viewmodel.SharedViewModel;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
 
 /**
  * Sets up the UI as a MediaPlayer/ExoPlayer of Step.videoUrl, the recipe's step instructions a.k.a.
@@ -64,8 +86,25 @@ import com.example.full_dream.baking.viewmodel.SharedViewModel;
  */
 public class StepDetailFragment extends Fragment {
 
+    private static final String KEY_PLAY_WHEN_READY = "play_when_ready";
+    private static final String KEY_WINDOW = "window";
+    private static final String KEY_POSITION = "position";
+
     // ViewModel(s)
     private SharedViewModel mSharedViewModel;
+
+    // MediaPlayer
+    private SimpleExoPlayer mExoPlayer;
+
+    private Timeline.Window mWindow;
+    private DataSource.Factory mMediaDataSourceFactory;
+    private DefaultTrackSelector mTrackSelector;
+    private boolean mShouldAutoPlay;
+    private BandwidthMeter mBandwidthMeter;
+
+    private boolean mPlayWhenReady;
+    private int mCurrentWindow;
+    private long mPlaybackPosition;
 
     /**
      * Called to do the initial creation of the fragment.
@@ -80,6 +119,24 @@ public class StepDetailFragment extends Fragment {
         // Use ViewModelProviders to associate an instance of SharedViewModel scoped with the
         // lifecycle of the UIController MainActivity
         mSharedViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+
+        if(savedInstanceState == null){
+            mPlayWhenReady = true;
+            mCurrentWindow = 0;
+            mPlaybackPosition = 0;
+        } else {
+            mPlayWhenReady = savedInstanceState.getBoolean(KEY_PLAY_WHEN_READY);
+            mCurrentWindow = savedInstanceState.getInt(KEY_WINDOW);
+            mPlaybackPosition = savedInstanceState.getLong(KEY_POSITION);
+        }
+
+        mShouldAutoPlay = true;
+        mBandwidthMeter = new DefaultBandwidthMeter();
+        mMediaDataSourceFactory = new DefaultDataSourceFactory(
+                getContext(),                                               // Context
+                Util.getUserAgent(getContext(), "baking"),    // Context, Application Name
+                (TransferListener<? super DataSource>) mBandwidthMeter);
+        mWindow = new Timeline.Window();
     }
 
     /**
@@ -96,8 +153,104 @@ public class StepDetailFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        FragmentStepDetailBinding binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_step_detail,
+                container,
+                false);
+
+//        if(Util.SDK_INT > 23){
+//            initializePlayer();
+//        } else {
+//            initializePlayer();
+//
+//            binding.playerview.setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//        }
+
+        binding.playerview.requestFocus();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(mBandwidthMeter);
+        mTrackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), mTrackSelector);
+        // Setup PlayerView by passing the SimpleExoPlayer instance to the PlayerView
+        binding.playerview.setPlayer(mExoPlayer);
+
+        mExoPlayer.setPlayWhenReady(mShouldAutoPlay);
+
+//        String blah = mSharedViewModel.getSelectedRecipe().getSteps().getId;
+        String blah="https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9a6_2-mix-sugar-crackers-creampie/2-mix-sugar-crackers-creampie.mp4";
+        MediaSource mediaSource = new ExtractorMediaSource
+                .Factory(mMediaDataSourceFactory)
+                .createMediaSource(Uri.parse(blah));
+
+        boolean haveStartPosition = mCurrentWindow != C.INDEX_UNSET;
+        if(haveStartPosition){
+            mExoPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
+        }
+
+        mExoPlayer.prepare(mediaSource, !haveStartPosition, false);
+
+        return binding.getRoot();
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
 
+        if(Util.SDK_INT <= 23){
+            releasePlayer();
+        } else {
+            releasePlayer();
+        }
+    }
+
+    /**
+     * Create a new instance of SimpleExoPlayer.
+     *  Convenient, default, all-purpose implementation of the ExoPlayer interface, which is the
+     *  public interface of the actual player in the ExoPlayer library.
+     *
+     * Comment Source:
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#2
+     */
+    private void initializePlayer(){
+
+        // Pass default components to the factory method newSimpleInstance and assign the returned
+        // instance to the member field mExoPlayer
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(getContext()),
+                new DefaultTrackSelector(),
+                new DefaultLoadControl());
+
+        // Setup resumption
+        mExoPlayer.setPlayWhenReady(mPlayWhenReady);
+        mExoPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
+
+        // Create a MediaSource 2
+//        String blah = "https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9a6_2-mix-sugar-crackers-creampie/2-mix-sugar-crackers-creampie.mp4";
+//        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+//        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(blah), mMediaDataSourceFactory,)
+
+        // Create a MediaSource 1
+//        Uri uri = Uri.parse(mSharedViewModel.getSelectedRecipe().getSteps().get(1).getVideoURL());
+//        MediaSource mediaSource = buildMediaSource(uri);
+//        mExoPlayer.prepare(mediaSource, true, false);
+    }
+
+    private void releasePlayer(){
+        if(mExoPlayer != null){
+            mPlaybackPosition = mExoPlayer.getCurrentPosition();
+            mCurrentWindow = mExoPlayer.getCurrentWindowIndex();
+            mPlayWhenReady = mExoPlayer.getPlayWhenReady();
+            mShouldAutoPlay = mExoPlayer.getPlayWhenReady();
+            mExoPlayer.release();
+            mExoPlayer = null;
+            mTrackSelector = null;
+        }
+    }
 }
