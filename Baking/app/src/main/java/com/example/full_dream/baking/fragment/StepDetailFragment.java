b7b4d3042e3272e359947a460b40ae7e9a50bf88
autumngoat <wong.sung.jeh@gmail.com>
2018-08-13 12:54:26 -0400

    P4 refactor: Update JDK from 1.7 to 1.8. Update ExoPlayer from 2.7.2 to 2.7.3. Update minSdkVersion from 15 to 19.
    Add comments and sources for ExoPlayer code.
    Modularize ExoPlayer code in StepDetailFragment.

--- Baking/app/src/main/java/com/example/full_dream/baking/fragment/StepDetailFragment.java
+++ Baking/app/src/main/java/com/example/full_dream/baking/fragment/StepDetailFragment.java
@@ -64,8 +64,6 @@ import com.example.full_dream.baking.viewmodel.SharedViewModel;
 
 // 3rd Party Imports - com - ExoPlayer2
 import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
@@ -91,24 +89,33 @@ public class StepDetailFragment extends Fragment {
     private static final String KEY_WINDOW = "window";
     private static final String KEY_POSITION = "position";
 
+    // Databinding View access so I can access the player view outside of onCreateView
+    FragmentStepDetailBinding mBinding;
+
     // ViewModel(s)
     private SharedViewModel mSharedViewModel;
 
-    // MediaPlayer
+    // Player
     private SimpleExoPlayer mExoPlayer;
 
+    // Player and MediaSource setup variables
     private Timeline.Window mWindow;
     private DataSource.Factory mMediaDataSourceFactory;
     private DefaultTrackSelector mTrackSelector;
     private boolean mShouldAutoPlay;
     private BandwidthMeter mBandwidthMeter;
 
+    // Playback variables
     private boolean mPlayWhenReady;
     private int mCurrentWindow;
     private long mPlaybackPosition;
 
     /**
      * Called to do the initial creation of the fragment.
+     *
+     * Source:
+     *  https://android.jlelse.eu/android-exoplayer-starters-guide-6350433f256c
+     *  https://google.github.io/ExoPlayer/guide.html
      *
      * @param savedInstanceState If the fragment is being re-created from a previous
      *                           saved state, this is the state.
@@ -121,22 +128,30 @@ public class StepDetailFragment extends Fragment {
         // lifecycle of the UIController MainActivity
         mSharedViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
 
+        // If there is no save state, then initialize playback values
         if(savedInstanceState == null){
             mPlayWhenReady = true;
             mCurrentWindow = 0;
             mPlaybackPosition = 0;
-        } else {
+        }
+        // If there is save state, then load the saved playback values
+        else {
             mPlayWhenReady = savedInstanceState.getBoolean(KEY_PLAY_WHEN_READY);
             mCurrentWindow = savedInstanceState.getInt(KEY_WINDOW);
             mPlaybackPosition = savedInstanceState.getLong(KEY_POSITION);
         }
 
+        // Play media on true and pause media on false
         mShouldAutoPlay = true;
+        // Measures bandwidth during playback.
+        //  Can be null if not required.
         mBandwidthMeter = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded
         mMediaDataSourceFactory = new DefaultDataSourceFactory(
                 getContext(),                                               // Context
                 Util.getUserAgent(getContext(), "baking"),    // Context, Application Name
                 (TransferListener<? super DataSource>) mBandwidthMeter);
+        //
         mWindow = new Timeline.Window();
     }
 
@@ -155,99 +170,184 @@ public class StepDetailFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        FragmentStepDetailBinding binding = DataBindingUtil.inflate(
+        mBinding = DataBindingUtil.inflate(
                 inflater,
                 R.layout.fragment_step_detail,
                 container,
                 false);
 
-//        if(Util.SDK_INT > 23){
-//            initializePlayer();
-//        } else {
-//            initializePlayer();
-//
-//            binding.playerview.setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LOW_PROFILE
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-//        }
+        mBinding.playerview.requestFocus();
 
-        binding.playerview.requestFocus();
-        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(mBandwidthMeter);
-        mTrackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-        mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), mTrackSelector);
-        // Setup PlayerView by passing the SimpleExoPlayer instance to the PlayerView
-        binding.playerview.setPlayer(mExoPlayer);
-
-        mExoPlayer.setPlayWhenReady(mShouldAutoPlay);
-
-//        String blah="https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9a6_2-mix-sugar-crackers-creampie/2-mix-sugar-crackers-creampie.mp4";
-        String blah = mSharedViewModel.getSelectedStep().getVideoURL();
-        MediaSource mediaSource = new ExtractorMediaSource
-                .Factory(mMediaDataSourceFactory)
-                .createMediaSource(Uri.parse(blah));
-
-        boolean haveStartPosition = mCurrentWindow != C.INDEX_UNSET;
-        if(haveStartPosition){
-            mExoPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
-        }
-
-        mExoPlayer.prepare(mediaSource, !haveStartPosition, false);
-
-        return binding.getRoot();
+        return mBinding.getRoot();
     }
 
+    /**
+     * If the API level is 24 or higher, then initialize the player and bind the player to the view.
+     *  Starting with API level 24 supports multiple windows.
+     *   As the app can be visible but not active in split windows mode, need to initialize the
+     *   player in onStart().
+     *
+     * Source:
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#2
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if(Util.SDK_INT > 23){
+
+            initializePlayer();
+
+            // Bind the player to the View
+            mBinding.playerview.setPlayer(mExoPlayer);
+        }
+    }
+
+    /**
+     * If the API level 23 or less, then initialize the player and bind the player to the view.
+     *  Before API level 24, best practice was to wait as long as possible to grab resources, so
+     *  wait until onResume() before initializing the player.
+     *
+     * Source:
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#2
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        hideSystemUi();
+
+        if(Util.SDK_INT <= 23){
+            initializePlayer();
+
+            // Bind the player to the View
+            mBinding.playerview.setPlayer(mExoPlayer);
+        }
+    }
+
+    /**
+     * Release the player when app is out of focus.
+     *  Before API level 24, there was no guarantee of onStop() being called, so there is a need to
+     *  release the player as early as possible in onPause().
+     *
+     * Source:
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#2
+     */
     @Override
     public void onPause() {
         super.onPause();
 
         if(Util.SDK_INT <= 23){
             releasePlayer();
-        } else {
+        }
+    }
+
+    /**
+     * Starting in API level 24, onStop() is guaranteed to be called and in the paused mode could
+     * still be visible since API level 24 introduced multi and split window modes.
+     *  Need to wait until onStop() to release the player.
+     *
+     * Source:
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#2
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if(Util.SDK_INT > 24){
             releasePlayer();
         }
     }
 
     /**
-     * Create a new instance of SimpleExoPlayer.
-     *  Convenient, default, all-purpose implementation of the ExoPlayer interface, which is the
-     *  public interface of the actual player in the ExoPlayer library.
+     * Create a new instance of SimpleExoPlayer to set to the player View.
      *
      * Comment Source:
-     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#2
+     *  https://google.github.io/ExoPlayer/guide.html
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#4
      */
     private void initializePlayer(){
+        if(mExoPlayer == null){
+            // Adaptive video playback cuts video and audio files into chunks of a given duration
+            //  A player then links them together for playback
+            //  The chunks are available in different qualities (size or bitrate)
+            //   With this, a player can choose the quality of each chunk according to the network
+            //   bandwidth available
+            //    After starting with a low quality chunk to be able to render the first frame as
+            //    quickly as possible, the player switches to a better quality for the second chunk
+            //    if sufficient bandwidth is available (for instance on wifi vs mobile)
+            //     At the heart of adaptive streaming is selecting the most appropriate TRACK to
+            //     adapt to the environment in which streaming happens
 
-        // Pass default components to the factory method newSimpleInstance and assign the returned
-        // instance to the member field mExoPlayer
-        mExoPlayer = ExoPlayerFactory.newSimpleInstance(
-                new DefaultRenderersFactory(getContext()),
-                new DefaultTrackSelector(),
-                new DefaultLoadControl());
+            // The factory method creates an AdaptiveVideoTrackSelection
+            TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(mBandwidthMeter);
+            // Selects a track
+            mTrackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+            // Create the player
+            //  newSimpleInstance(Context, TrackSelector) returns a SimpleExoPlayer which extends
+            //  ExoPlayer to add additional high level player functionality
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), mTrackSelector);
 
-        // Setup resumption
-        mExoPlayer.setPlayWhenReady(mPlayWhenReady);
-        mExoPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
+            // Starts and pauses playback
+            mExoPlayer.setPlayWhenReady(mShouldAutoPlay);
+            boolean haveStartPosition = mCurrentWindow != C.INDEX_UNSET;
+            if(haveStartPosition){
+                // Seek within the MediaSource
+                mExoPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
+            }
 
-        // Create a MediaSource 2
-//        String blah = "https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9a6_2-mix-sugar-crackers-creampie/2-mix-sugar-crackers-creampie.mp4";
-//        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-//        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(blah), mMediaDataSourceFactory,)
-
-        // Create a MediaSource 1
-//        Uri uri = Uri.parse(mSharedViewModel.getSelectedRecipe().getSteps().get(1).getVideoURL());
-//        MediaSource mediaSource = buildMediaSource(uri);
-//        mExoPlayer.prepare(mediaSource, true, false);
+            // Grab and prepare the MediaSource
+            String videoUrl = mSharedViewModel.getSelectedStep().getVideoURL();
+            // Prepare the player with the source
+            mExoPlayer.prepare(buildMediaSource(videoUrl), !haveStartPosition, false);
+        }
     }
 
+    /**
+     * Create a MediaSource by loading the media from provided Uri to the DataSource.
+     *
+     * Source:
+     *  https://google.github.io/ExoPlayer/guide.html
+     *
+     * @param videoSource Video URL from Step.videoURL.
+     * @return MediaSource representing the media to be played
+     */
+    private MediaSource buildMediaSource(String videoSource){
+        MediaSource mediaSource = new ExtractorMediaSource
+                .Factory(mMediaDataSourceFactory)
+                .createMediaSource(Uri.parse(videoSource));
+
+        return mediaSource;
+    }
+
+    /**
+     * Implementation detail to have a full screen experience.
+     *
+     * Source:
+     *  https://codelabs.developers.google.com/codelabs/exoplayer-intro/index.html?index=..%2F..%2Findex#2
+     */
+    private void hideSystemUi(){
+        mBinding.playerview.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    /**
+     * Release the player when it is no longer needed while saving the playback state.
+     *
+     * Source:
+     *  https://google.github.io/ExoPlayer/guide.html
+     */
     private void releasePlayer(){
         if(mExoPlayer != null){
             mPlaybackPosition = mExoPlayer.getCurrentPosition();
             mCurrentWindow = mExoPlayer.getCurrentWindowIndex();
             mPlayWhenReady = mExoPlayer.getPlayWhenReady();
+            // Grab play/pause state
             mShouldAutoPlay = mExoPlayer.getPlayWhenReady();
             mExoPlayer.release();
             mExoPlayer = null;
